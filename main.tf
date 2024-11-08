853dd2ecadec09b75d2ad4c41e4303b716263b1f
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 17:07:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "google_compute_network" "vpc_network" {
-  project                 = "gcp-host"
+  project                 = "gcp-host-441110"
   name                    = "host-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
