dec87b8516191bd6e254fb8d83ead1d7ebcee22e
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 17:38:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,50 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges-0
   }
   depends_on = [ google_compute_network.vpc_network ]
 }
+
+
+# Creating Virtual Machine
+
+resource "google_compute_instance" "default" {
+  name         = "dev-test-001"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  
+  network_interface {
+    network = "host-vpc"
+    subnetwork = "test-subnet-001"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email = "gcp-jenkins@dev-gce-test.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
+
 /*
 resource "google_compute_shared_vpc_host_project" "host" {
   project = "dev-gce-test"
