cbe0116f592160e71f30fe5396b242537a81e558
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 16:53:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ resource "google_storage_bucket" "google_storage_bucket-003" {
 }
 
 
-/*
+
 resource "google_compute_network" "vpc_network" {
-  project                 = "dev-gce-test"
+  project                 = "gcp-host"
   name                    = "host-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
-} */
+} 
 
