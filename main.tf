d1c12232ddaefcbfcbcd8944e2f343ca0a61e82e
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 17:08:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+resource "google_storage_bucket" "google_storage_bucket-003" {
+  name          = "infra-cicd-bucket-jenkins-001"
+  location      = "US"
+  project       = "dev-gce-test"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
 
 resource "google_compute_network" "vpc_network" {
   project                 = "gcp-host-441110"
