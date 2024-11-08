349eb74edb42d2365e642150658df0e109dd78a4
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 16:43:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,19 +8,14 @@ resource "google_storage_bucket" "google_storage_bucket-003" {
 }
 
 
-terraform {
- backend "gcs" {
-   bucket = "infra-cicd-bucket-jenkins-001"
-   
-             
- }
-}
 
-/*
+
+
+
 resource "google_compute_network" "vpc_network" {
   project                 = "gcp-host"
   name                    = "host-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
-*/
+
