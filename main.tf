abe1737bc7963dd2b3b67a2f0641d4922ada459d
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-08 16:40:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "google_storage_bucket-003" {
-  name          = "infra-cicd-bucket-jenkins-011"
+  name          = "infra-cicd-bucket-jenkins-001"
   location      = "US"
   project       = "dev-gce-test"
   force_destroy = true
@@ -10,7 +10,7 @@ resource "google_storage_bucket" "google_storage_bucket-003" {
 
 terraform {
  backend "gcs" {
-   bucket = "infra-cicd-bucket-jenkins-011"
+   bucket = "infra-cicd-bucket-jenkins-001"
    
              
  }
