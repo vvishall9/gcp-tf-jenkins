81c03755f9efdf8238f2bd701dd38d002511fedd
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2024-11-18 13:35:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges-0
 
 
 # Creating Virtual Machine
-
+/*
 resource "google_compute_instance" "default" {
   name         = "dev-test-002"
   project       = "dev-gce-test"
@@ -83,7 +83,7 @@ resource "google_compute_instance" "default" {
     scopes = ["cloud-platform"]
   }
 }
-
+*/
 /*
 resource "google_compute_shared_vpc_host_project" "host" {
   project = "dev-gce-test"
