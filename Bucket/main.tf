3935a78958d5134aca12f34a1677d980caa53531
Vishal Kumar <kumarvisha@hcltech.com>
2025-08-22 15:00:57 +0530

    Adding bucket

--- Bucket/main.tf
+++ Bucket/main.tf
@@ -7,8 +7,8 @@ resource "google_storage_bucket" "google_storage_bucket-088" {
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "google_storage_bucket-009" {
-  name          = "infra-cicd-bucket-jenkins-091"
+resource "google_storage_bucket" "google_storage_bucket-089" {
+  name          = "infra-cicd-bucket-jenkins-097"
   location      = "US"
   project       = "hcl2025"
   force_destroy = true
