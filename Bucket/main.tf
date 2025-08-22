aa090870bebaa4df8c99c1c5c6c61e3b120d8b16
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2025-08-22 19:00:07 +0530

    Update main.tf

--- Bucket/main.tf
+++ Bucket/main.tf
@@ -6,5 +6,14 @@ resource "google_storage_bucket" "google_storage_bucket-001" {
 
   public_access_prevention = "enforced"
 }
+resource "google_storage_bucket" "google_storage_bucket-002" {
+  name          = "infra-cicd-bucket-jenkins-gcp"
+  location      = "US"
+  project       = "cloud-factory-gcp-platform"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
 
 
