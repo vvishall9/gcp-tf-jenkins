e98953cf6fe80ff5fb3b8cadcb2b9f5dea6b2c72
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2025-08-23 08:33:39 +0530

    Update main.tf

--- Compute/main.tf
+++ Compute/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "vm_instance" {
     name = "simple-vm-01"
     machine_type = "e2-micro"
     zone = "asia-south1-c"
-    project  = "hcl2025"
+    project  = "cloud-factory-gcp-platform"
 
 boot_disk {
     initialize_params {
