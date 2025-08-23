9cb5297a67bd569de56649c4948a4992a80f93b0
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2025-08-23 09:33:25 +0530

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = "hcl2025"
+  project                 = "cloud-factory-gcp-platform"
   name                    = "host-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
@@ -9,7 +9,7 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   name          = "test-subnet-001"
   ip_cidr_range = "10.2.0.0/28"
   region        = "us-central1"
-  project       = "hcl2025"
+  project       = "cloud-factory-gcp-platform"
   network       = google_compute_network.vpc_network.id
   secondary_ip_range {
     range_name    = "tf-test-secondary-range-update1"
