7c7ff7f7071e6e34e692e829130260c881a7307d
Vishal Kumar <75535269+vvishall9@users.noreply.github.com>
2025-08-23 09:35:52 +0530

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -1,3 +1,4 @@
+/*
 resource "google_compute_network" "vpc_network" {
   project                 = "cloud-factory-gcp-platform"
   name                    = "host-vpc"
@@ -17,4 +18,6 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   }
   depends_on = [ google_compute_network.vpc_network ]
 }
+
+*/
 
